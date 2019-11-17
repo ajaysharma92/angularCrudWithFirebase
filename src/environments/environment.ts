971516b55e3ea6d8160e2d051f34fd1f5e07750f
ajaysharma92 <sharmajay121@gmail.com>
2019-11-18 02:12:06 +0530

    Completed CRUD

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,21 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyDqBpP4yjeuIV0QE8P21iYKjrxxjd_5-xY",
+    authDomain: "angularcrudwithfirebase-9d843.firebaseapp.com",
+    databaseURL: "https://angularcrudwithfirebase-9d843.firebaseio.com",
+    projectId: "angularcrudwithfirebase-9d843",
+    storageBucket: "angularcrudwithfirebase-9d843.appspot.com",
+    messagingSenderId: "186677745488",
+    appId: "1:186677745488:web:d6ab4d25840e62af15be55",
+    measurementId: "G-5VC65XVPKZ"
+  }
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
